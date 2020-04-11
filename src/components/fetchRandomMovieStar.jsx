688861ccf0464290a6b9cc4b5e377f2e133fcd99
Peter Jones <peteflux@gmail.com>
2020-04-11 17:42:25 -0400

    i can pull the img path in api call but the path renders and not the actual image

--- src/components/fetchRandomMovieStar.jsx
+++ src/components/fetchRandomMovieStar.jsx
@@ -17,14 +17,26 @@ class FetchRandomMovieStar extends React.Component {
         const url = `https://api.themoviedb.org/3/person/popular?api_key=0923dd9b4328f2ddced216cb32ecf851&language=en-US&page=${randomPage}`;
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        const randomActor = randomPageFunction(1, 20);
+
+        this.setState({
+            star: data.results[randomActor].profile_path, loading: false
+        });
+        console.log(data.results[randomActor].profile_path);
     };
 
     render() {
         return(
             <div>
-                {this.state.loading ? <div>loading...</div> : <div>star...</div>
-                    }
+                {this.state.loading || !this.state.star ? (
+                <div>loading...</div> 
+                ) : (
+                <div>
+                    <div>
+                        {this.state.star}
+                    </div>
+                </div>
+                )}
             </div>
         )
     }
