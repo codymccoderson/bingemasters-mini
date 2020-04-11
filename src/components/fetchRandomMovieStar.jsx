9b4c29974c1ac5161e76af116962cff4f84be18b
Peter Jones <peteflux@gmail.com>
2020-04-11 15:13:52 -0400

    random api page call works

--- src/components/fetchRandomMovieStar.jsx
+++ src/components/fetchRandomMovieStar.jsx
@@ -10,11 +10,11 @@ class FetchRandomMovieStar extends React.Component {
 
     async componentDidMount() {
         const randomPageFunction = function randomPage(min, max) {
-            return Math.random() * (max - min) + min;
+            return Math.round(Math.random() * (max - min) + min);
         };
         const randomPage = randomPageFunction(1, 500);
         console.log(randomPage);
-        const url = "https://api.themoviedb.org/3/person/popular?api_key=0923dd9b4328f2ddced216cb32ecf851&language=en-US&page={randomPage}";
+        const url = `https://api.themoviedb.org/3/person/popular?api_key=0923dd9b4328f2ddced216cb32ecf851&language=en-US&page=${randomPage}`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
