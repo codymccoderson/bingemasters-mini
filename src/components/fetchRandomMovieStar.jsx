4e5ba112d912a4ef9301fff0c0b650835c9d6d9f
Peter Jones <peteflux@gmail.com>
2020-04-11 14:53:47 -0400

    working on randomizing page selection on api call

---
+++ src/components/fetchRandomMovieStar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class FetchRandomMovieStar extends React.Component {
+
+    state = {
+        loading: true,
+        star: null
+
+    };
+
+    async componentDidMount() {
+        const randomPageFunction = function randomPage(min, max) {
+            return Math.random() * (max - min) + min;
+        };
+        const randomPage = randomPageFunction(1, 500);
+        console.log(randomPage);
+        const url = "https://api.themoviedb.org/3/person/popular?api_key=0923dd9b4328f2ddced216cb32ecf851&language=en-US&page={randomPage}";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    };
+
+    render() {
+        return(
+            <div>
+                {this.state.loading ? <div>loading...</div> : <div>star...</div>
+                    }
+            </div>
+        )
+    }
+
+};
+
+export default FetchRandomMovieStar;
