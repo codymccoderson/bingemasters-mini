033238bb9b7adfd3db48cd8935e33e6c226deb6c
Peter Jones <peteflux@gmail.com>
2020-04-13 22:03:47 -0400

    I made a timer and it works. Not sure how to work it into the game correctly yet though

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import FetchRandomMovieStar from './components/fetchRandomMovieStar';
+import FetchRandomMovieStar from './components/FetchRandomMovieStar';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <h1>Bingemasters</h1>
       <FetchRandomMovieStar/>
+      <Timer startCount='15'/>
     </div>
   );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class Timer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 15
+    }
+  }
+
+  render () {
+    const {count} = this.state
+    return (
+      <div>
+        <h1>Current Count: {count}</h1>
+      </div>
+    )
+  }
+  
+  componentDidMount () {
+    const {startCount} = this.props
+    this.setState({
+      count: startCount
+    })
+    this.doIntervalChange();
+  }
+
+  doIntervalChange = () => {
+      this.myInterval = setInterval(() => {
+        if (this.state.count > 0)
+      this.setState(prevState => ({
+        count: prevState.count - 1
+      }))
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.myInterval)
+  }
+}
+
+export default Timer
