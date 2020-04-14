2732367e3b5ca3afd0f1ddf0c8c732197fe4ece1
Peter Jones <peteflux@gmail.com>
2020-04-14 19:08:13 -0400

    added conditionals to try and sort out promise glitch to no avail

--- src/utils/getRandomPage.js
+++ src/utils/getRandomPage.js
@@ -6,8 +6,16 @@ const getRandomPage = async () => {
         const randomPage = randomizer(1, 500);
         const url = `https://api.themoviedb.org/3/person/popular?api_key=0923dd9b4328f2ddced216cb32ecf851&language=en-US&page=${randomPage}`;
         const data = await loadData(url); 
-        const actor = data.results[randomizer(1, 20)]
-        return actor;
+        const actor = data.results[randomizer(1, 20)];
+        const randomActorPhotoPath = actor.profile_path || "" || undefined || null;
+        const movieTheyWereIn = actor.known_for[0].title || "" || undefined || null;
+        const secondMovieTheyWereIn = actor.known_for[1].title || "" || undefined || null;
+        if (randomActorPhotoPath === actor.profile_path && movieTheyWereIn === actor.known_for[0].title && secondMovieTheyWereIn === actor.known_for[1].title) {
+            return actor;
+        } else {
+            getRandomPage();
+        }
+        
 } 
 
 export default getRandomPage;
