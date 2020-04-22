a756d7a9cf23da66cecebeda0cdf44988cedb117
Peter Jones <peteflux@gmail.com>
2020-04-22 09:40:05 -0400

    additional security with gitignore

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
+import keys from '../config'
 
  
  
@@ -10,7 +11,7 @@ const responseGoogle = (response) => {
  
 ReactDOM.render(
   <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    clientId={keys.googleClientID}
     buttonText="Login"
     onSuccess={responseGoogle}
     onFailure={responseGoogle}
