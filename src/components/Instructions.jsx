3e9da04de21e3746e9f2cdfc99c174b6438767a9
Peter Jones <peteflux@gmail.com>
2020-04-23 16:30:57 -0400

    added some more styles to the instructions component and it is now responsive

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -25,83 +25,121 @@ const StyledStartGame = styled.div`
     margin: 0 auto;
     padding-left: 0;
     position: relative;
-    top: 180px;
+    top: 150px;
     right: 150px;
 
-    @media screen and (max-width: 1380px) {
-        margin: 0 590px;
+    @media screen and (max-width: 1430px) {
+        margin: 0 390px;
+        width: 700px;
+        height: 700px;
 
     }
-    @media screen and (max-width: 1320px) {
-        margin: 0 545px;
+
+    @media screen and (max-width: 1400px) {
+        margin: 0 380px;
+        width: 660px;
+        height: 660px;
+
+    }
+    @media screen and (max-width: 1390px) {
+        margin: 0 370px;
+        width: 620px;
+        height: 620px;
     }
 
-    @media screen and (max-width: 1180px) {
-        margin: 0 515px;
+    @media screen and (max-width: 1340px) {
+        margin: 0 370px;
+        width: 580px;
+        height: 580px;
     }
 
-    @media screen and (max-width: 1180px) {
-        margin: 0 500px;
+    @media screen and (max-width: 1290px) {
+        margin: 0 360px;
+        width: 540px;
+        height: 540px;
     }
 
-    @media screen and (max-width: 1080px) {
-        margin: 0 480px;
+    @media screen and (max-width: 1240px) {
+        margin: 0 360px;
+        width: 500px;
+        height: 500px;
     }
 
-    @media screen and (max-width: 950px) {
-        margin: 0 435px;
+    @media screen and (max-width: 1190px) {
+        margin: 0 380px;
+        width: 460px;
+        height: 460px;
     }
 
-    @media screen and (max-width: 900px) {
-        margin: 0 400px;
+    @media screen and (max-width: 1140px) {
+        margin: 0 380px;
+        width: 420px;
+        height: 420px;
+    }
+
+    @media screen and (max-width: 1090px) {
+        margin: 0 340px;
+        width: 420px;
+        height: 420px;
+    }
+
+    @media screen and (max-width: 1040px) {
+        margin: 0 340px;
+        width: 400px;
+        height: 400px;
+    }
+
+    @media screen and (max-width: 990px) {
+        margin: 0 330px;
+        width: 380px;
+        height: 380px;
+    }
+
+    @media screen and (max-width: 940px) {
+        margin: 0 330px;
+        width: 360px;
+        height: 360px;
+    }
+
+    @media screen and (max-width: 890px) {
+        margin: 0 320px;
+        width: 340px;
+        height: 340px;
     }
 
     @media screen and (max-width: 850px) {
-        margin: 0 450px;
-        display: flex;
-        align-items: center;
-        flex-flow: column;
-        width: 200px;
-        height: 200px;
-        padding-left: 0;
-        position: relative;
-        top: 230px;
-    }
-
-    @media screen and (max-width: 800px) {
-        margin: 0 410px;
-    }
-
-    @media screen and (max-width: 750px) {
-        margin: 0 400px;
-    }
-
-    @media screen and (max-width: 700px) {
-        margin: 0 380px;
-    }
-
-    @media screen and (max-width: 675px) {
-        margin: 0 350px;
-    }
-
-    @media screen and (max-width: 650px) {
-        margin: 0 340px;
-    }
-
-    @media screen and (max-width: 625px) {
-        margin: 0 330px;
-    }
-
-    @media screen and (max-width: 600px) {
         margin: 0 320px;
+        width: 480px;
+        height: 480px;
     }
 
-    @media screen and (max-width: 585px) {
-        margin: 0 310px;
+    @media screen and (max-width: 840px) {
+        margin: 0 280px;
+
     }
 
-    @media screen and (max-width: 575px) {
-        margin: 0 300px;
+    @media screen and (max-width: 790px) {
+        margin: 0 260px;
+        width: 460px;
+        height: 460px;
+    }
+
+    @media screen and (max-width: 740px) {
+        margin: 0 240px;
+        width: 440px;
+        height: 440px;
+    }
+
+    @media screen and (max-width: 690px) {
+        margin: 0 220px;
+        width: 420px;
+        height: 420px;
+    }
+
+    @media screen and (max-width: 640px) {
+        margin: 0 200px;
+        width: 400px;
+        height: 400px;
     }
 `;
 
@@ -109,13 +147,50 @@ const HowToPlayTitle = styled.h1`
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     font-size: 48px;
     text-align: center;
+
+    @media screen and (max-width: 1400px) {
+        font-size: 42px;
+    }
+
+    @media screen and (max-width: 850px) {
+        font-size: 42px;
+        color: #666666;
+        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    }
 `;
 
 const WhatTheGameIs = styled.h2`
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    font-size: 18px;
+    font-size: 24px;
+
+    @media screen and (max-width: 1400px) {
+        font-size: 22px;
+    }
+
+    @media screen and (max-width: 850px) {
+        font-size: 22px;
+        font-weight: bolder;
+        color: #b7e6f0;
+        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    }
+    
 `
 
+const Text = styled.p`
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 18px;
+
+    @media screen and (max-width: 1400px) {
+        font-size: 16px;
+    }
+
+    @media screen and (max-width: 850px) {
+        font-size: 16px;
+        font-weight: bolder;
+        color: pink;
+        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    }
+`
 
 const ButtonLine = styled.div`
     display: flex;
@@ -212,7 +287,6 @@ const ReturnHomeButton = styled.button`
 
 `;
 
-
 class Instructions extends React.Component {
 
     state = {
@@ -237,20 +311,28 @@ class Instructions extends React.Component {
                             How To Play
                         </HowToPlayTitle>
                         <WhatTheGameIs>
-                            Bingemasters is a single player trivia game where you guess the name of the actor that appears on screen.
+                            Bingemasters is a single player trivia game where players guess the name of the actor that appears on screen.
                         </WhatTheGameIs>
                         <ul>
                             <li>
-                                <p>Players have 45 seconds to guess the actor in question.</p>
+                                <Text>
+                                    Players have 45 seconds to guess the actor in question.
+                                </Text>
                             </li>
                             <li>
-                                <p>Players must correctly spell the actor's name in the input field to score a point.</p>
+                                <Text>
+                                    Players must correctly spell the actor's name in the input field to score a point.
+                                </Text>
                             </li>
                             <li>
-                                <p>The goal of the game is to record the highest streak of correct answers and the highest streaks of all-time will be kept on the global leaderboard.</p>
+                                <Text>
+                                    The goal of the game is to record the highest streak of correct answers and the highest streaks of all-time will be kept on the global leaderboard.
+                                </Text>
                             </li>
                             <li>
-                                <p>The game will end if your timer reaches zero. But don't fret! You can just play again!</p>
+                                <Text>
+                                    The game will end if your timer reaches zero. But don't fret! You can just play again!
+                                </Text>
                             </li>
                         </ul>
                     </div>
